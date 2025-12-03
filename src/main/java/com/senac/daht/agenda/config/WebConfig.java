99db4268cf66a380913e0701978e00ec72988573
Gugzz21 <gustavodinizz966@gmail.com>
2025-12-03 12:15:21 -0300

    Criação WebConfig

---
+++ src/main/java/com/senac/daht/agenda/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.senac.daht.agenda.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a todas as rotas
+                .allowedOrigins(
+                        "http://localhost:8081",             // Para desenvolvimento no navegador/web
+                        "http://academico3.rj.senac.br"      // Origem do servidor (pode ser redundante, mas seguro)
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*"); // Permite cabeçalhos customizados, como o 'Authorization'
+    }
+}
